4a9f0399c2cc72fa4d459e651e1f4b7a80c9a8c7
Diegofergamboa <>
2021-04-03 18:50:57 +0000

    The last version of this program is a reprogram version that the easy level beta and, the hard level. Using shuffle as a randomer of the list and using the append item to replace the =+ used in the last easy version

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised: 
-
-
-ls_letters = 0 
+total_passsword = []
+ 
 for ll in range(1, nr_letters + 1) :
-	ls_letters = random.choice(letters)
+	total_passsword.append(random.choice(letters))
 	
-ls_number = 0 
 for ln in range(1, nr_letters + 1) :
-	ln_number = random.choice(numbers)
+	total_passsword.append(random.choice(numbers))
 	
-ls_symbols = 0 
 for ll in range(1, nr_letters + 1) :
-	ls_symbols = random.choice(symbols)
+	total_passsword.append(random.choice(symbols))
 
-print(ls_letters,ls_number,ls_symbols)
+
+print(total_passsword)
+print('\n')
+print('\n')
+random.shuffle(total_passsword)
+print(total_passsword)
+
 
 
